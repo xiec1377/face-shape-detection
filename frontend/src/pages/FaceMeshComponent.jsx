069afde166b2cf13efad109cb36506499c9edf01
Catherine Xie <catherinexie2014@gmail.com>
2025-07-02 19:46:26 -0400

    chore: record landmark indices (e.g. cheek, nose, chin landmarks)

--- frontend/src/pages/FaceMeshComponent.jsx
+++ frontend/src/pages/FaceMeshComponent.jsx
@@ -34,6 +34,14 @@ function FaceMeshComponent() {
 
       if (results.multiFaceLandmarks) {
         for (const landmarks of results.multiFaceLandmarks) {
+          // console.log(landmarks);
+          console.log("chin:", landmarks[152])
+          console.log("left jaw:", landmarks[234])
+          console.log("right jaw:", landmarks[454])
+          console.log("forehead:", landmarks[10])
+          console.log("nose tip:", landmarks[1])
+          console.log("left cheek:", landmarks[93])
+          console.log("right cheek:", landmarks[323])
           for (const point of landmarks) {
             canvasCtx.beginPath();
             canvasCtx.arc(
